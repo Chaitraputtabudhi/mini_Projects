aa7b638bae0c4729461f6bafc6f0c2fd04e321f7
Chaitraputtabudhi <chaitraputtabudhi@gmail.com>
2025-07-08 12:18:04 -0700

    drums project

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,26 +1,38 @@
 for (let i =0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-        // alert("I got clicked!");
-        // var audio = new Audio("sounds/tom-1.mp3");
-        // audio.play();
-        switch (this.innerHTML) {
-            case "w":    new Audio("sounds/tom-1.mp3").play();
-                break;
-            case "a":    new Audio("sounds/tom-2.mp3").play();
-                break;
-            case "s": new Audio("sounds/tom-3.mp3").play();
-                break;
-            case "d": new Audio("sounds/tom-4.mp3").play(); 
-                break;
-            case "j": new Audio("sounds/snare.mp3").play();
-                break;
-            case "k": new Audio("sounds/crash.mp3").play();
-                break;
-            case "l": new Audio("sounds/kick-bass.mp3").play();
-                break;
-            default: console.log(this.innerHTML);}
-    });
-        
+        MakeSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });        
 }
 
+document.addEventListener("keydown", function(event){    
+    MakeSound(event.key);
+    buttonAnimation(event.key);
+})
 
+function MakeSound(key){
+    switch (key) {
+        case "w":    new Audio("sounds/tom-1.mp3").play();
+            break;
+        case "a":    new Audio("sounds/tom-2.mp3").play();
+            break;
+        case "s": new Audio("sounds/tom-3.mp3").play();
+            break;
+        case "d": new Audio("sounds/tom-4.mp3").play(); 
+            break;
+        case "j": new Audio("sounds/snare.mp3").play();
+            break;
+        case "k": new Audio("sounds/crash.mp3").play();
+            break;
+        case "l": new Audio("sounds/kick-bass.mp3").play();
+            break;
+        default: console.log(key);}
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
