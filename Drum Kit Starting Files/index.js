56f47f07b1f1bfe2d90720845695a16d7015d4f7
Chaitraputtabudhi <chaitraputtabudhi@gmail.com>
2025-07-08 11:22:55 -0700

    day 8

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,26 @@
+for (let i =0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        // alert("I got clicked!");
+        // var audio = new Audio("sounds/tom-1.mp3");
+        // audio.play();
+        switch (this.innerHTML) {
+            case "w":    new Audio("sounds/tom-1.mp3").play();
+                break;
+            case "a":    new Audio("sounds/tom-2.mp3").play();
+                break;
+            case "s": new Audio("sounds/tom-3.mp3").play();
+                break;
+            case "d": new Audio("sounds/tom-4.mp3").play(); 
+                break;
+            case "j": new Audio("sounds/snare.mp3").play();
+                break;
+            case "k": new Audio("sounds/crash.mp3").play();
+                break;
+            case "l": new Audio("sounds/kick-bass.mp3").play();
+                break;
+            default: console.log(this.innerHTML);}
+    });
+        
+}
+
+
