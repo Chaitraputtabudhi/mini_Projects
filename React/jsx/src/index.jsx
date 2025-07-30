649928224fcc1f7b467cf5d0f33a9d420053a9f8
Chaitraputtabudhi <chaitraputtabudhi@gmail.com>
2025-07-30 14:19:17 -0700

    React and jsx initial code

---
+++ React/jsx/src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const name = "Mohan Kumar";
+const hour = new Date().getHours();
+const time = hour < 12 ? "Morning" : hour < 17 ? "Afternoon" : "Evening";
+const color = hour < 12 ? "red" : hour < 17 ? "green" : "blue";
+
+
+
+// root.render(
+//   <div>
+//     <p>Created by {name}</p>
+//     <p>Copyright {new Date().getFullYear()}</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+root.render(
+    <h1 style={{color}}>Good {time}</h1>,document.getElementById("root")
+)
